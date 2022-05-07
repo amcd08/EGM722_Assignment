95e30f3b14e1d77f5a7edf25bd96ddc169653025
Anita <anitadonaghy@gmail.com>
2022-05-07 19:05:37 +0100

    Doc string added to funtion

--- Nearest_ASSI.py
+++ Nearest_ASSI.py
@@ -1,10 +1,16 @@
 from shapely.strtree import STRtree
 import geopandas as gpd
 
-
 def get_nearest_assi_to_sch(sch_name, assis_ferm, my_schs):
-    assis_tree = STRtree(assis_ferm['geometry'].values) # creates an STRtree object with the ASSI data
+    '''
 
+    :param sch_name: Enter the full name of the school
+    :param assis_ferm: the Areas of Special Scientific Interest in Co Fermanagh
+    :param my_schs: the schools point shapefile
+    :return: returns selected information on the nearest ASSI to each School
+    '''
+
+    assis_tree = STRtree(assis_ferm['geometry'].values) # creates an STRtree object with the ASSI data
     Fschs = my_schs.loc[my_schs['Name'] == sch_name, 'geometry'].values[0]
     nearest = assis_tree.nearest(Fschs) # gets the closest ASSI  to each sch
 
@@ -18,4 +24,25 @@ myschs = gpd.read_file('data_files/schoolF_points.shp')
 nearest_row = get_nearest_assi_to_sch(sch_name, assis_ferm, myschs)
 print(f'Name: {nearest_row.NAME.values[0]}, Type: {nearest_row.Type.values[0]}, Habitat: {nearest_row.HABITAT.values[0]}, Species: {nearest_row.SPECIESPT1.values[0]}')
 
+####
+def complete(text,state):
 
+    schls = [myschs('Name')]
+    results = [x for x in schls if x.startswith(text)] + [None]
+    return results[state]
+
+readline.set_completer(complete)
+line = input('prompt> ')
+
+
+import readline
+readline.parse_and_bind("tab: complete")
+
+def complete(text,state):
+    volcab = ['dog','cat','rabbit','bird','slug','snail']
+    results = [x for x in volcab if x.startswith(text)] + [None]
+    return results[state]
+
+readline.set_completer(complete)
+
+line = input('prompt> ')
